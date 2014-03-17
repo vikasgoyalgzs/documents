93f2813029ec2a53bb3c1151e310194f29fc3c3d
Vikas Goyal <vikasgoyalgzs@gmail.com>
2014-03-17 13:59:26 -0400

    Extensions

--- cppjkneekbjaeellbfkmgnhonkkjfpdn/0.3.3.3_0/js/load-default-options.js
+++
@@ -1,5 +0,0 @@
-(function(){
-	localStorage['timeperiod']		= localStorage['timeperiod'] || "last_hour";
-	localStorage['data_to_remove']	= localStorage['data_to_remove'] || JSON.stringify({"cache":true});
-	localStorage['cookie_settings']	= localStorage['cookie_settings']  || JSON.stringify({"inclusive":true,"filters":[]});
-})();
